543a25ad4e54cec993af133e271eaccbb8d47cb3
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2023-04-14 02:46:48 +0800

    Add files via upload

---
+++ (Balderas, Gobui, Nacario, Ochavo, Suico) - CIS 1202 - Final Project - (MW 730-1000)/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Marjhun's Apartelle</title>
+        <link rel="shortcut icon" href="./website_favicon.png" type="image/x-icon">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/5253132cc4.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+    
+            <div class="title_bar">
+                <ul>
+                    <li><img src="./website_favicon.png" alt="" id="brand_logo"></li>
+                    <li><h2 id="brand_name">Marjhun's Apartelle</h2></li>
+                    <li>
+
+                        <div class="user_profile">
+                            <img src="./user_profile.png" alt="" id="user_pic">
+                            <h5>LorenzSuico215</h5>
+
+                            <div class="user_menu">
+                                <ul>
+                                    <a href="#"><li><i class="fa-solid fa-gear"></i>Account Setting</li></a>
+                                    <a href="#"><li><i class="fa-solid fa-right-from-bracket"></i>Logout Account</li></a>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </li>
+                </ul>
+            </div>
+
+            <nav class="nav_bar">
+                <ul>
+                    <a href="#"><li id="first_item"><i class="fa-solid fa-house"></i>Home</li></a>
+                    <a href="#"><li><i class="fa-solid fa-calendar-day"></i>Room Booking</li></a>
+                    <a href="#"><li><i class="fa-solid fa-calendar-check"></i>Activity Scheduling</li></a>
+                    <a href="#"><li><i class="fa-solid fa-star"></i>Customer Review</li></a>
+                    <a href="#"><li><i class="fa-solid fa-people-roof"></i>About Us</li></a>
+                </ul>
+            </nav>
+
+        </header>
+
+    </body>
+</html>
